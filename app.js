4e10a14f601f4a30fe2d4357c2a52983bdf40c8a
Talvarez <seba.alvarez16@gmail.com>
2013-02-12 05:36:40 -0300

    Se corrige fallo en horarios.js, estaba devolviendo mal los horarios a Once

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ horarios.actualizar(function(trenes) {
 	console.log(trenes.proximoMoreno('LINIERS'));
 	console.log(trenes.siguienteMoreno('LINIERS'));
 
+	console.log(trenes.proximoOnce('LINIERS'));
+	console.log(trenes.siguienteOnce('LINIERS'));
+
 	//Simple debug
 	console.log(trenes);
 
